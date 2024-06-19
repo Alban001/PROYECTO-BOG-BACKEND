4db8d569da7d98feb642f6ee824d9565de1abbf3
Elias Alban <Alban001@users.noreply.github.com>
2024-06-19 15:03:46 -0400

    tercer cambio general

--- controllers/posts.js
+++ controllers/posts.js
@@ -15,7 +15,7 @@ export const getPosts = (req, res) => {
 
 export const getPost = (req, res) => {
   const q =
-    "SELECT p.id, `username`, `title`, `desc`, p.img, u.img AS userImg, `cat`,`date` FROM user u JOIN posts p ON u.id = p.uid WHERE p.id = ? ";
+    "SELECT p.id, `username`, `title`, `desc`, p.img, u.img AS userImg, `cat`,`date` FROM user u JOIN posts p ON u.id = p.userId WHERE p.id = ? ";
 
   db.query(q, [req.params.id], (err, data) => {
     if (err) return res.status(500).json(err);
@@ -32,14 +32,14 @@ export const addPost = (req, res) => {
     if (err) return res.status(403).json("Token no válido!");
 
     const q =
-      "INSERT INTO posts(`title`, `desc`, `img`, `cat`, `date`,`userId`) VALUES (?)";
+      "INSERT INTO posts(`title`, `desc`, `img`,`date`, `cat`, `userId`) VALUES (?)";
 
     const values = [
       req.body.title,
       req.body.desc,
       req.body.img,
-      req.body.cat,
       req.body.date,
+      req.body.cat,
       userInfo.id,
     ];
 
--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "../frontend/public/upload");
@@ -19,12 +20,15 @@ const storage = multer.diskStorage({
   },
 });
 
+
+
 const upload = multer({ storage });
 
 app.post("/servidor/upload", upload.single("file"), function (req, res) {
   const file = req.file;
   res.status(200).json(file.filename);
 });
+
 
 app.use("/servidor/auth", authRoutes);
 app.use("/servidor/users", userRoutes);
