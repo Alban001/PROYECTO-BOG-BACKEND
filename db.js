a99575e2049a24b17a3abf3957377dea0fc8bea1
Elias Alban <Alban001@users.noreply.github.com>
2024-06-25 07:33:12 -0400

    server changes

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 import mysql from 'mysql'
 
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "blog_cruz"
+    host: 'sql.freedb.tech',
+    user: 'freedb_jose_cruz',
+    password:'#D2AVarNqQ#SZb*',
+    database: 'freedb_blog_cruz'
 })
