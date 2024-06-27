67601c96d9578e174f7b79032b1c427b98e9618f
Elias Alban <Alban001@users.noreply.github.com>
2024-06-27 03:46:37 -0400

    clou module

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import cookieParser from "cookie-parser";
 import multer from "multer";
+const cloudinary = require("cloudinary").v2
 const app = express();
 import cors from 'cors'
 
@@ -12,23 +13,25 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
+const upload = multer({dest : "uploads"});
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "https://oxefalublog.netlify.app/public/upload");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, Date.now() + file.originalname);
+//   },
+// });
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "https://oxefalublog.netlify.app/public/upload");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + file.originalname);
-  },
-});
 
-
-
-const upload = multer({ storage });
-
-app.post("/servidor/upload", upload.single("file"), function (req, res) {
-  const file = req.file;
-  res.status(200).json(file.filename);
+app.post("/upload", upload.single("file"), async (req, res)=> {
+      try{
+        const result = await cloudinary.uploader.upload(req.file.path);
+        res.status(200).json(result)
+      }catch(error){
+          console.log("error",error)
+          res.status(400).send(error.message)
+      }
 });
 
 
